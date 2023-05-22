72fac05c850777783611bd53be1dc99e23773689
fai <s10243483@connect.np.edu.sg>
2023-05-22 19:45:26 +0800

    plz work

--- app/src/main/java/sg/edu/np/mad/week2t04/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/week2t04/MainActivity.java
@@ -2,23 +2,31 @@ package sg.edu.np.mad.week2t04;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.icu.text.CaseMap;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     final String TITLE = "Main Activity";
-
+    private int generateRanNum(){
+        Random ran = new Random();
+        int myNumber = ran.nextInt(999999);
+        return myNumber;
+    }
     User user1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.v(TITLE, "On Create!");
-        user1 = new User("Chun Fai", "blahhhhhhhhh");
+        user1 = new User("MAD "+generateRanNum(), "[Text]");
         TextView boldtext = findViewById(R.id.textView2);
         TextView normtext = findViewById(R.id.textView3);
         boldtext.setText(user1.name);
@@ -50,12 +58,23 @@ public class MainActivity extends AppCompatActivity {
                 if (myUser.followed == false){
                     myUser.followed = true;
                     followButton.setText("Unfollow");
+                    Toast.makeText(getApplicationContext(), "Followed", Toast.LENGTH_SHORT).show();
                 }
                 else{
                     myUser.followed = false;
                     followButton.setText("Follow");
+                    Toast.makeText(getApplicationContext(), "Unfollowed", Toast.LENGTH_SHORT).show();
                 }
                 Log.v(TITLE,"Button A is pressed");
+            }
+        });
+
+        Button messageButton = findViewById(R.id.button2);
+        messageButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent myIntent1 = new Intent(MainActivity.this, MessageGroup.class);
+                startActivity(myIntent1);
             }
         });
     }
