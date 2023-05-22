448e7bf3319a4b9e190caf9af4628a33407e1052
fai <s10243483@connect.np.edu.sg>
2023-05-22 20:07:45 +0800

    plz work111

--- app/src/main/java/sg/edu/np/mad/week2t04/ListActivity.java
+++ app/src/main/java/sg/edu/np/mad/week2t04/ListActivity.java
@@ -26,7 +26,7 @@ public class ListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_list);
         Log.v(title, "Create");
     }
-    //a
+    //aa
     @Override
     protected void onStart(){
         super.onStart();
